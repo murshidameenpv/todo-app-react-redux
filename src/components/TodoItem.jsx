521667800c4a010200505163db7a419a378ffbc0
murshidameen <murshidmonzpv@gmail.com>
2024-02-19 16:33:10 +0530

    final all futilities implemented

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
-import { FaCheck, FaToggleOff, FaToggleOn } from "react-icons/fa";
+import { FaCheck,  } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { deleteTodo,markAsCompleted,markAsIncomplete } from "../redux/todoSlice.js";
+import { useDispatch } from "react-redux";
 
 const TodoItem = ({ index, todo }) => {
+  const dispatch = useDispatch();
   return (
     <li className="flex flex-col sm:flex-row sm:items-center justify-between border-b-2 py-2 gap-4">
       <div className="flex items-center">
@@ -11,14 +15,25 @@ const TodoItem = ({ index, todo }) => {
         <span>{todo.text}</span>
       </div>
       <div className="space-x-3 ml-8">
-        <button className="text-sm mr-2 bg-blue-700 rounded sm:px-2 py-1 px-1 text-white">
-          <FaToggleOff />
-          <FaToggleOn />
-        </button>
-        <button className="text-sm mr-2 bg-green-700 rounded sm:px-2 py-1 px-1 text-white">
-          <FaCheck />
-        </button>
-        <button className="text-sm mr-2 bg-red-700 rounded sm:px-2 py-1 px-1 text-white">
+        {todo.isCompleted ? (
+          <button
+            className="text-sm mr-2 bg-yellow-600 rounded sm:px-2 py-1 px-1 text-white"
+            onClick={() => dispatch(markAsIncomplete(todo.id))}
+          >
+            <ImCross />
+          </button>
+        ) : (
+          <button
+            className="text-sm mr-2 bg-green-700 rounded sm:px-2 py-1 px-1 text-white"
+            onClick={() => dispatch(markAsCompleted(todo.id))}
+          >
+            <FaCheck />
+          </button>
+        )}
+        <button
+          className="text-sm mr-2 bg-red-700 rounded sm:px-2 py-1 px-1 text-white"
+          onClick={() => dispatch(deleteTodo(todo.id))}
+        >
           <RiDeleteBin6Line />
         </button>
       </div>
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -10,6 +10,21 @@ const todoSlice = createSlice({
       console.log(action, "ooooooooo");
       state.push({ id: Date.now(), text: action.payload, isCompleted: false });
     },
+    deleteTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+    markAsCompleted: (state, action) => {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.isCompleted = true;
+      }
+    },
+    markAsIncomplete: (state, action) => {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.isCompleted = false;
+      }
+    },
     markAllCompleted: (state) => {
       return state.map((todo) => ({ ...todo, isCompleted: true }));
     },
@@ -17,5 +32,5 @@ const todoSlice = createSlice({
 });
 console.log(todoSlice, "pppppppppppppppp");
 
-export const { addTodo, markAllCompleted } = todoSlice.actions;
+export const { addTodo, markAllCompleted, deleteTodo ,markAsCompleted,markAsIncomplete} = todoSlice.actions;
 export default todoSlice.reducer;
